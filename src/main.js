69fd82c00778d064052504b1f8b19f5199aab62f
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-03-18 15:16:03 +0700

    create tooltip

--- src/main.js
+++ src/main.js
@@ -2,31 +2,71 @@ $(document).ready(function () {
     $(".abrev-text-1").html($(".lang").val());
     $(".abrev-text-2").html($(".country").val());
     $(".abrev-text-3").html($(".currency").val());
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip({
-            trigger: "hover",
-        });
-    });
-    selectpicker();
+
+    let langSelectorElm = document.querySelector(".selectpick.lang");
+    let selectedLang =
+        langSelectorElm.options[langSelectorElm.selectedIndex].innerHTML;
+    let countrySelectorElm = document.querySelector(".selectpick.country");
+    let selectedCountry =
+        countrySelectorElm.options[countrySelectorElm.selectedIndex].innerHTML;
+    let currencySelectorElm = document.querySelector(".selectpick.currency");
+    let selectedCurrency =
+        currencySelectorElm.options[currencySelectorElm.selectedIndex]
+            .innerHTML;
+
+    selectpicker(selectedLang, selectedCountry, selectedCurrency);
 });
 
-async function selectpicker() {
+async function selectpicker(selectedLang, selectedCountry, selectedCurrency) {
     await $(".selectpick").selectpicker();
-    console.log($(".btn"));
+    $(".btn").attr("data-tooltip", "tooltip");
+    $(".lang-container .btn").attr(
+        "data-original-title",
+        "Language : " + selectedLang
+    );
+    $(".country-container .btn").attr(
+        "data-original-title",
+        "Coutry : " + selectedCountry
+    );
+
+    $(".currency-container .btn").attr(
+        "data-original-title",
+        "Currency : " + selectedCurrency
+    );
+    $(".btn").attr("data-placement", "top");
+    $('[data-tooltip="tooltip"]').tooltip({
+        trigger: "hover",
+    });
 }
 
 $(".lang").change(function (e) {
     $(".abrev-text-1").html(e.target.value);
+    let title = e.target.options[e.target.selectedIndex].innerHTML;
+    $(".lang-container .btn").attr(
+        "data-original-title",
+        "Language : " + title
+    );
     e.preventDefault();
 });
 
 $(".country").change(function (e) {
     $(".abrev-text-2").html(e.target.value);
+    let title = e.target.options[e.target.selectedIndex].innerHTML;
+    $(".country-container .btn").attr(
+        "data-original-title",
+        "Coutry : " + title
+    );
+
     e.preventDefault();
 });
 
 $(".currency").change(function (e) {
     $(".abrev-text-3").html(e.target.value);
+    let title = e.target.options[e.target.selectedIndex].innerHTML;
+    $(".currency-container .btn").attr(
+        "data-original-title",
+        "Currency : " + title
+    );
     e.preventDefault();
 });
 
