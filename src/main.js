67b4262096954e92f6dee795a0a65d024da180fa
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-07-19 20:17:25 +0700

    add modal

--- src/main.js
+++ src/main.js
@@ -85,3 +85,58 @@ $(".accordion").on("show.bs.collapse", function (e) {
 $(".accordion").on("hide.bs.collapse", function (e) {
     $(e.target).parent().find(".arrow-icon").first().removeClass("rotate");
 });
+
+$("#copy-btn").tooltip({
+    trigger: "click",
+});
+
+//copy to clipboard
+$("#copy-btn").click(function () {
+    $("#copy-btn").tooltip("show");
+    copyTextToClipboard($(this).attr("data-copy"));
+    tooltipTimeout();
+});
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand("copy");
+        var msg = successful ? "successful" : "unsuccessful";
+        console.log("Fallback: Copying text command was " + msg);
+    } catch (err) {
+        console.error("Fallback: Oops, unable to copy", err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(
+        function () {
+            console.log("Async: Copying to clipboard was successful!");
+        },
+        function (err) {
+            console.error("Async: Could not copy text: ", err);
+        }
+    );
+}
+
+function tooltipTimeout() {
+    setTimeout(function () {
+        $("#copy-btn").tooltip("hide");
+    }, 1000);
+}
